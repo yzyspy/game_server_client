f54f523cd498b360ced33f7b87da1dae0b84c215
杨忠宇 <kane.yang@jaco.live>
2025-09-03 11:44:48 +0800

    fix
    Signed-off-by: 杨忠宇 <kane.yang@jaco.live>

--- server/znet/request.go
+++ server/znet/request.go
@@ -7,6 +7,8 @@ import (
 type Request struct {
 	conn ziface.IConnection // the connection which has been established with the client(已经和客户端建立好的连接)
 	msg  ziface.IMessage    // the request data sent by the client(客户端请求的数据)
+
+	data []byte // the response data to the client(响应给客户端的数据)
 }
 
 func (r *Request) GetConnection() ziface.IConnection {
@@ -14,6 +16,5 @@ func (r *Request) GetConnection() ziface.IConnection {
 }
 
 func (r *Request) GetData() []byte {
-	//TODO implement me
-	panic("implement me")
+	return r.data
 }
