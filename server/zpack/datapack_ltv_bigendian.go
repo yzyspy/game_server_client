c2d7e93170cab48b56a564ba7b41dcea9e6a5c81
杨忠宇 <kane.yang@jaco.live>
2025-09-07 08:31:33 +0800

    fix
    Signed-off-by: 杨忠宇 <kane.yang@jaco.live>

--- server/zpack/datapack_ltv_bigendian.go
+++ server/zpack/datapack_ltv_bigendian.go
@@ -1,1 +1,22 @@
 package zpack
+
+import (
+	"zinx-server/ziface"
+)
+
+type DataPack struct{}
+
+func (d *DataPack) GetHeadLen() uint32 {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *DataPack) Pack(msg ziface.IMessage) ([]byte, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *DataPack) Unpack(bytes []byte) (ziface.IMessage, error) {
+	//TODO implement me
+	panic("implement me")
+}
--- server/zpack/datapack_ltv_littleendian.go
+++ server/zpack/datapack_ltv_littleendian.go
@@ -1,1 +1,20 @@
 package zpack
+
+import "zinx-server/ziface"
+
+type DataPackLtv struct{}
+
+func (d *DataPackLtv) GetHeadLen() uint32 {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *DataPackLtv) Pack(msg ziface.IMessage) ([]byte, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *DataPackLtv) Unpack(bytes []byte) (ziface.IMessage, error) {
+	//TODO implement me
+	panic("implement me")
+}
